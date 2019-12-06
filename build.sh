f0240f59f0fa48d0dc6f736b789036e37924bab6
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-06 14:56:07 +1000

    Switched to https://github.com/gkatzioura/CloudStorageMaven/tree/master/S3StorageWagon

--- build.sh
+++ build.sh
@@ -10,27 +10,28 @@ cp geckodriver firefox
 pushd firefox
 tar -cvf ../firefoxbundle.tar.bz2 *
 popd
-wget https://repo1.maven.org/maven2/org/springframework/build/aws-maven/5.0.0.RELEASE/aws-maven-5.0.0.RELEASE.jar
+wget https://repo1.maven.org/maven2/com/gkatzioura/maven/cloud/s3-storage-wagon/2.3/s3-storage-wagon-2.3.jar
+wget https://repo1.maven.org/maven2/com/gkatzioura/maven/cloud/cloud-storage-core/2.3/cloud-storage-core-2.3.jar
 wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-core/1.11.687/aws-java-sdk-core-1.11.687.jar
 wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-s3/1.11.687/aws-java-sdk-s3-1.11.687.jar
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.10.1/jackson-databind-2.10.1.jar
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.10.1/jackson-core-2.10.1.jar
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.10.1/jackson-annotations-2.10.1.jar
-#wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.10/httpclient-4.5.10.jar
-wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.5/httpclient-4.5.5.jar
+wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.10/httpclient-4.5.10.jar
 wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpcore/4.4.12/httpcore-4.4.12.jar
 wget https://repo1.maven.org/maven2/joda-time/joda-time/2.10.5/joda-time-2.10.5.jar
-cp aws-maven-5.0.0.RELEASE.jar apache-maven-3.6.3/lib
+cp s3-storage-wagon-2.3.jar apache-maven-3.6.3/lib
+cp cloud-storage-core-2.3.jar apache-maven-3.6.3/lib
 cp aws-java-sdk-core-1.11.687.jar apache-maven-3.6.3/lib
 cp aws-java-sdk-s3-1.11.687.jar apache-maven-3.6.3/lib
 cp jackson-databind-2.10.1.jar apache-maven-3.6.3/lib
 cp jackson-core-2.10.1.jar apache-maven-3.6.3/lib
 cp jackson-annotations-2.10.1.jar apache-maven-3.6.3/lib
-cp httpclient-4.5.5.jar apache-maven-3.6.3/lib
+cp httpclient-4.5.10.jar apache-maven-3.6.3/lib
 cp httpcore-4.4.12.jar apache-maven-3.6.3/lib
 cp joda-time-2.10.5.jar apache-maven-3.6.3/lib
 mkdir ~/.m2
-echo "<settings><servers><server><id>octopus-maven-repo</id><username>$AWS_ACCESS_KEY</username><password>$AWS_SECRET_KEY</password></server></servers></settings>" > ~/.m2/settings.xml
+echo "<settings><servers><server><id>octopus-maven-repo</id><username>$AWS_ACCESS_KEY</username><password>$AWS_SECRET_KEY</password><configuration><region>us-east-1</region><publicRepository>false</publicRepository></configuration></server></servers></settings>" > ~/.m2/settings.xml
 ./apache-maven-3.6.3/bin/mvn deploy:deploy-file \
   -Dfile=firefoxbundle.tar.bz2 \
   -DgroupId=com.octopus.linux \
@@ -38,4 +39,4 @@ echo "<settings><servers><server><id>octopus-maven-repo</id><username>$AWS_ACCES
   -Dversion=72.0 \
   -Dpackaging=tar.bz2 \
   -DrepositoryId=octopus-maven-repo \
-  -Durl=s3://octopus-maven-repo
+  -Durl=s3://octopus-maven-repo/release

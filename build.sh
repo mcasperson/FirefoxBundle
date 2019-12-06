08b0d9d79d3eac48df71c3edbe91525e22943cf7
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-06 14:19:07 +1000

    Trying a different httpclient version because of https://stackoverflow.com/questions/24139352/aws-s3-java-sdk-access-denied

--- build.sh
+++ build.sh
@@ -16,7 +16,8 @@ wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-s3/1.11.687/aws-j
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-databind/2.10.1/jackson-databind-2.10.1.jar
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.10.1/jackson-core-2.10.1.jar
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.10.1/jackson-annotations-2.10.1.jar
-wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.10/httpclient-4.5.10.jar
+#wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.10/httpclient-4.5.10.jar
+wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.5/httpclient-4.5.5.jar
 wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpcore/4.4.12/httpcore-4.4.12.jar
 wget https://repo1.maven.org/maven2/joda-time/joda-time/2.10.5/joda-time-2.10.5.jar
 cp aws-maven-5.0.0.RELEASE.jar apache-maven-3.6.3/lib
@@ -25,7 +26,7 @@ cp aws-java-sdk-s3-1.11.687.jar apache-maven-3.6.3/lib
 cp jackson-databind-2.10.1.jar apache-maven-3.6.3/lib
 cp jackson-core-2.10.1.jar apache-maven-3.6.3/lib
 cp jackson-annotations-2.10.1.jar apache-maven-3.6.3/lib
-cp httpclient-4.5.10.jar apache-maven-3.6.3/lib
+cp httpclient-4.5.5.jar apache-maven-3.6.3/lib
 cp httpcore-4.4.12.jar apache-maven-3.6.3/lib
 cp joda-time-2.10.5.jar apache-maven-3.6.3/lib
 mkdir ~/.m2

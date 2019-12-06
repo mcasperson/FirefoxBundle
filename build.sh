82bcde6757338fe930c302b3d54116317869db9a
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-06 13:39:47 +1000

    Added more libraries

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-core/2.10
 wget https://repo1.maven.org/maven2/com/fasterxml/jackson/core/jackson-annotations/2.10.1/jackson-annotations-2.10.1.jar
 wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpclient/4.5.10/httpclient-4.5.10.jar
 wget https://repo1.maven.org/maven2/org/apache/httpcomponents/httpcore/4.4.12/httpcore-4.4.12.jar
+wget https://repo1.maven.org/maven2/joda-time/joda-time/2.10.5/joda-time-2.10.5.jar
 cp aws-maven-5.0.0.RELEASE.jar apache-maven-3.6.3/lib
 cp aws-java-sdk-core-1.11.687.jar apache-maven-3.6.3/lib
 cp aws-java-sdk-s3-1.11.687.jar apache-maven-3.6.3/lib
@@ -26,6 +27,7 @@ cp jackson-core-2.10.1.jar apache-maven-3.6.3/lib
 cp jackson-annotations-2.10.1.jar apache-maven-3.6.3/lib
 cp httpclient-4.5.10.jar apache-maven-3.6.3/lib
 cp httpcore-4.4.12.jar apache-maven-3.6.3/lib
+cp joda-time-2.10.5.jar apache-maven-3.6.3/lib
 mkdir ~/.m2
 echo "<settings><servers><server><id>octopus-maven-repo</id><username>$AWS_ACCESS_KEY</username><password>$AWS_SECRET_KEY</password></server></servers></settings>" > ~/.m2/settings.xml
 ./apache-maven-3.6.3/bin/mvn deploy:deploy-file \
